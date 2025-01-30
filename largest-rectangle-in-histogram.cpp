3e4318dccedb5f650ce2b78a9f010e884359bf2f
kashish00208 <kgupta7827@gmail.com>
2025-01-30 16:00:56 +0530

    DSA

--- largest-rectangle-in-histogram.cpp
+++ largest-rectangle-in-histogram.cpp
@@ -47,3 +47,29 @@ public:
         return ans;
     }
 };
+
+
+//OPTIMAL SOLUTION - Single passs
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int ans = 0 , n = heights.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int element = st.top();
+                st.pop();
+                int nse = i , pse = st.empty()?-1:st.top();
+                ans = max(ans , heights[element]*(nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int nse = n , element = st.top();
+            st.pop();
+            int pse = st.empty()?-1:st.top();
+            ans = max(ans , heights[element]*(nse-pse-1));
+        }
+        return ans;
+    }
+};
