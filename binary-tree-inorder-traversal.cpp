d651a5ef6aa308589f8d4c94859a72579eb26282
kashish00208 <kgupta7827@gmail.com>
2025-02-24 00:18:54 +0530

    DSA

---
+++ binary-tree-inorder-traversal.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        void get(TreeNode* root,vector<int>&ans){
+            if(root==NULL)return;
+            get(root->left,ans);
+            ans.push_back(root->val);
+            get(root->right,ans);
+        }
+        vector<int> inorderTraversal(TreeNode* root) {
+            vector<int>ans;
+            get(root,ans);
+            return ans;
+        }
+    };
