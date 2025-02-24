b67f5980bd0ac6c7623dc87128c1290953b4416c
kashish00208 <141568392+kashish00208@users.noreply.github.com>
2025-02-24 18:33:33 +0530

    Update binary-tree-preorder-traversal.cpp

--- binary-tree-preorder-traversal.cpp
+++ binary-tree-preorder-traversal.cpp
@@ -23,3 +23,27 @@ class Solution {
             return ans;
         }
     };
+//Iterative approach to solve the problem
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (!root) {
+            return {};
+        }
+        vector<int>ans;
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode*temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->right){
+                st.push(temp->right);
+            }
+            if(temp->left){
+                st.push(temp->left);
+            }
+        }
+        return ans;                          
+    }
+};
